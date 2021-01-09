2d2388783f056876bab3545638473a1e0ca91709
y2sec <bae3835@gmail.com>
2021-01-09 17:58:17 +0900

    AppConfig 추가를 통한 DIP, OCP 위반 사항 해결

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
