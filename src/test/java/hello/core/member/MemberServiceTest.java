186e40a26bf332aa8c59df637a653f5733b2a33d
y2sec <bae3835@gmail.com>
2021-01-09 17:56:29 +0900

    AppConfig 추가를 통한 DIP, OCP 위반 사항 해결

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,19 @@
 package hello.core.member;
 
-import org.junit.jupiter.api.Assertions;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
