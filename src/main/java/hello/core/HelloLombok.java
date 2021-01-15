edc2a6b9094b3d55b64f52b97de51d7f759ebf54
y2sec <bae3835@gmail.com>
2021-01-15 17:57:01 +0900

    Lombok 추가

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+}
